de0ed18594805b6beb76b2270a6525af9ed188a1
Octavio Simone <o.simone@dracoon.com>
2020-10-09 11:06:09 +0200

    minor fix

--- user_csv_example.py
+++ user_csv_example.py
@@ -11,9 +11,9 @@ import getpass
 import csv
 import os 
 
-clientID = 'dracoon_legacy_scripting' # replace with client id from OAuth app - for Cloud you can use dracoon_legacy_scripting if enabled in apps
+clientID = 'xxxx' # replace with client id from OAuth app - for Cloud you can use dracoon_legacy_scripting if enabled in apps
 clientSecret = 'xxxxxx' # replace with client secret - dracoon_legacy_scripting has no secret, it can be omitted as parameter 
-baseURL = 'https://demo-os.dracoon.com' # replace with own DRACOON url
+baseURL = 'https://dracoon.team' # replace with own DRACOON url
 
 # create DRACOON object 
 my_dracoon = core.Dracoon(clientID)
