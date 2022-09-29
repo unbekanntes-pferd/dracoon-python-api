5e0f732cd00c07dea4007fde70b2fc346bdd5401
Octavio Simone <o.simone@dracoon.com>
2022-09-29 15:51:46 +0200

    migrate groups test to unittest

--- tests/test_e2e_groups.py
+++ tests/test_e2e_groups.py
@@ -1,11 +1,12 @@
-
 import os
 import asyncio
+import unittest
 import dotenv
 from dracoon import DRACOONClient, OAuth2ConnectionType
 from dracoon.groups import DRACOONGroups
 from dracoon.groups.models import CreateGroup, UpdateGroup
-from dracoon.groups.responses import Group, GroupList, GroupUserList, LastAdminGroupRoomList
+from dracoon.groups.responses import Group, GroupList, GroupUserList, LastAdminGroupRoomList, UserType
+from dracoon.user import DRACOONUser
 from dracoon.user.responses import RoleList
 
 dotenv.load_dotenv()
@@ -15,78 +16,134 @@ client_secret = os.environ.get('CLIENT_SECRET')
 username = os.environ.get('E2E_USER_NAME')
 password = os.environ.get('E2E_PASSWORD')
 base_url = os.environ.get('E2E_BASE_URL')
-USER_ID = 795 # replace with existing user id
 
-async def test_groups_e2e():
-
-    dracoon = DRACOONClient(base_url=base_url, client_id=client_id, client_secret=client_secret)
-    await dracoon.connect(OAuth2ConnectionType.password_flow, username=username, password=password)
-    groups = DRACOONGroups(dracoon_client=dracoon)
-    assert isinstance(groups, DRACOONGroups)
-    print('Connection test complete (/)')
-
-
-    group_list = await groups.get_groups()
-    assert isinstance(group_list, GroupList)
-    print('Getting user list test passed (/)')
-
-    new_group = groups.make_group(name='THIS IS A TEST')
-    assert isinstance(new_group, CreateGroup)
-
-    print('Making group create payload test passed (/)')
-
-    group = await groups.create_group(new_group)
-    assert isinstance(group, Group)
-
-    print('Creating a group test passed (/)')
-
-    update = groups.make_group_update(name='THIS IS ANOTHER TEST')
-    assert isinstance(update, UpdateGroup)
-
-    print('Making group update payload test passed (/)')
-
-    updated_group = await groups.update_group(group_id=group.id, group_update=update)
-    assert updated_group.name == 'THIS IS ANOTHER TEST'
-    print('Updating a group test passed (/)')
-
-    group_roles = await groups.get_group_roles(group_id=group.id)
-    assert isinstance(group_roles, RoleList)
-    print('Getting group roles test passed (/)')
-
-    group_last_admin_rooms = await groups.get_group_last_admin_rooms(group_id=group.id)
-    assert isinstance(group_last_admin_rooms, LastAdminGroupRoomList)
-    print('Getting group last admin rooms test passed (/)')
+class TestAsyncDRACOONGroups(unittest.IsolatedAsyncioTestCase):
     
-    user_list = [USER_ID]
-    add_user = await groups.add_group_users(group_id=group.id, user_list=user_list)
-    assert isinstance(add_user, Group)
-    print('Adding user to group test passed (/)')
+    async def asyncSetUp(self) -> None:
+        asyncio.get_running_loop().set_debug(False)
+        
+        self.dracoon = DRACOONClient(base_url=base_url, client_id=client_id, client_secret=client_secret, raise_on_err=True)
+        await self.dracoon.connect(OAuth2ConnectionType.password_flow, username=username, password=password)
 
-    group_users = await groups.get_group_users(group_id=group.id)
-    assert isinstance(group_users, GroupUserList)
-    assert len(group_users.items) > 0
-    assert group_users.items[0].userInfo.id == USER_ID
-    print('Getting group users test passed (/)')
+        self.groups = DRACOONGroups(dracoon_client=self.dracoon)
+        assert isinstance(self.groups, DRACOONGroups)
+        
+        self.user = DRACOONUser(dracoon_client=self.dracoon)
+  
+    async def asyncTearDown(self) -> None:
+        await self.dracoon.logout()
+        
+        
+    async def test_get_groups(self):
+        group_list = await self.groups.get_groups()
+        assert isinstance(group_list, GroupList)
+        
+        
+    async def test_create_group(self):
+        new_group = self.groups.make_group(name='CREATE TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
 
-    del_group_user = await groups.delete_group_users(group_id=group.id, user_list=user_list)
-    assert isinstance(del_group_user, Group)
-    print('Deleting group users test passed (/)')
+        await self.groups.delete_group(group_id=group.id)
 
-    del_group = await groups.delete_group(group_id=group.id)
+    async def test_update_group(self):
+        new_group = self.groups.make_group(name='UPDATE TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        update_name = 'THIS IS ANOTHER TEST'
+        
+        update = self.groups.make_group_update(name=update_name)
+        
+        assert isinstance(update, UpdateGroup)
 
-    assert del_group == None
+        updated_group = await self.groups.update_group(group_id=group.id, group_update=update)
+        assert updated_group.name == update_name
+        
+        await self.groups.delete_group(group_id=group.id)
 
-    print('Deleting group test passed (/)')
+    async def test_get_group_roles(self):
 
-    print('Groups E2E tests passed (/)')
+        new_group = self.groups.make_group(name='GROUP ROLES TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        group_roles = await self.groups.get_group_roles(group_id=group.id)
+        assert isinstance(group_roles, RoleList)
+        
+        await self.groups.delete_group(group_id=group.id)
+    
+    async def test_get_add_group_users(self):
 
+        new_group = self.groups.make_group(name='GROUP USERS TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        user_info = await self.user.get_account_information()
+        
+        await self.groups.add_group_users(group_id=group.id, user_list=[user_info.id])
+        
+        group_users = await self.groups.get_group_users(group_id=group.id)
+        assert isinstance(group_users, GroupUserList)
+        assert len(group_users.items) == 1
+        group_user = group_users.items[0]
+        assert group_user.isMember == True
+        assert group_user.userInfo.lastName == user_info.lastName
+        assert group_user.userInfo.firstName == user_info.firstName
+        assert group_user.userInfo.email == user_info.email
+        assert group_user.userInfo.id == user_info.id
+        assert group_user.userInfo.userName == user_info.userName
+        assert group_user.userInfo.userType == UserType.internal
+                
+        await self.groups.delete_group(group_id=group.id)
+    
+    async def test_get_group_last_admins(self):
 
+        new_group = self.groups.make_group(name='GROUP LAST ADMIN TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        group_last_admin_rooms = await self.groups.get_group_last_admin_rooms(group_id=group.id)
+        assert isinstance(group_last_admin_rooms, LastAdminGroupRoomList)
+        
+        await self.groups.delete_group(group_id=group.id)
+        
+    async def test_delete_group_users(self):
+
+        new_group = self.groups.make_group(name='GROUP USERS DELETE TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        user_info = await self.user.get_account_information()
+        
+        await self.groups.add_group_users(group_id=group.id, user_list=[user_info.id])
+        
+        group_users = await self.groups.get_group_users(group_id=group.id)
+        assert len(group_users.items) == 1
+        
+        await self.groups.delete_group_users(group_id=group.id, user_list=[user_info.id])
+
+        group_users = await self.groups.get_group_users(group_id=group.id)
+        assert len(group_users.items) == 0
+
+        await self.groups.delete_group(group_id=group.id)
+    
+    async def test_delete_group(self):
+        
+        new_group = self.groups.make_group(name='DELETE TEST')
+        assert isinstance(new_group, CreateGroup)
+        group = await self.groups.create_group(new_group)
+        assert isinstance(group, Group)
+        
+        del_group = await self.groups.delete_group(group_id=group.id)
+        assert del_group == None
+        
 
 if __name__ == '__main__':
-    asyncio.run(test_groups_e2e())
-
-    
-
-   
-
-
+    unittest.main()
